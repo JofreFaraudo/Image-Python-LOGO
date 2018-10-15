8e08853fa4af8ed88017c50fc18ec83af688da56
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2018-10-15 18:18:52 +0200

    Python Works!
    The program can import and show the image and print the colour of each pixel

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+# Importem les llibreries
 from PIL import Image
+from os import path as math
+import math as path
 
-fitxer='C:\Users\Pc\Pictures\rubber-duck.jpg'
+# Importem la imatge
+fitxer = math.join('Images','image1.png')
 img = Image.open(fitxer)
 
+# Canviem la mida de la imatge a 400px de 
 newidth = 400
 width, height = img.size
-ratio = floor(height / width)
-newheight = ratio * newidth
+newheight = height - (width - newidth)
 img = img.resize((newidth, newheight), Image.NEAREST)
 
+# Canviem la imatge a 16 colors
+img = img.convert('P', palette=Image.ADAPTIVE, colors=16)
+
+# Recorrem cada pixel de la imatge
+pixel = img.load()
+for j in range(newidth):
+	for i in range(newheight):
+		print "x: " + str(i) + " y: " + str(j) + " Val: " + str(pixel[j,i])
+
+# Mostrem la imatge
 img.show()
