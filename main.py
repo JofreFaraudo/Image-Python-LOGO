45a317f8fe262f71ebd911c38f2bda3f9492f700
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2018-10-22 18:53:00 +0200

    First version works!

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# Importem les llibreries
+ # Importem les llibreries
 from PIL import Image
 from os import path as math
 import math as path
 
+# Creem fitxer de sortida i hi posem les comandes basiques
+out = open("code.lgo","a")
+out.write("setpensize 1")
+
 # Importem la imatge
 fitxer = math.join('Images','image1.png')
 img = Image.open(fitxer)
-
-# Canviem la mida de la imatge a 400px de 
-newidth = 400
+# Canviem la mida de la imatge a 400px
+newidth = 32 
 width, height = img.size
 newheight = height - (width - newidth)
 img = img.resize((newidth, newheight), Image.NEAREST)
@@ -20,7 +23,7 @@ img = img.convert('P', palette=Image.ADAPTIVE, colors=16)
 pixel = img.load()
 for j in range(newidth):
 	for i in range(newheight):
-		print "x: " + str(i) + " y: " + str(j) + " Val: " + str(pixel[j,i])
+		out.write(" setxy " + str(j) + " " + str(i) + " color " + str(16-pixel[j,i]) + " fd 1")
 
-# Mostrem la imatge
+		# Mostrem la imatge
 img.show()
