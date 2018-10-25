dc2b93732611d87548c2fab9c899a41f7293a032
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2018-10-25 18:06:04 +0200

    Second version
    lol

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from os import path as math
 import math as path
 
 # Creem fitxer de sortida i hi posem les comandes basiques
-out = open("code.lgo","a")
-out.write("setpensize 1")
+out = open("jaloiun.lgo","a")
+out.write("rt 90 setpensize 1")
 
 # Importem la imatge
-fitxer = math.join('Images','image1.png')
+fitxer = math.join('Images','jalouin.png')
 img = Image.open(fitxer)
+
 # Canviem la mida de la imatge a 400px
-newidth = 32 
+newidth = 200 
 width, height = img.size
 newheight = height - (width - newidth)
 img = img.resize((newidth, newheight), Image.NEAREST)
@@ -22,8 +23,16 @@ img = img.convert('P', palette=Image.ADAPTIVE, colors=16)
 # Recorrem cada pixel de la imatge
 pixel = img.load()
 for j in range(newidth):
+	# Inicialitzem variable per al color, zero es cap
+	actualcolor = 0
+	# Movem la tortuga al lloc desitjat
+	out.write("\nsetxy 0 " + str(j))
 	for i in range(newheight):
-		out.write(" setxy " + str(j) + " " + str(i) + " color " + str(16-pixel[j,i]) + " fd 1")
-
-		# Mostrem la imatge
-img.show()
+		if actualcolor != pixel[j,i]:
+			if actualcolor != 0:
+				out.write("\ncolor " + str(actualcolor) + "\nfd " + str(length*2))
+			actualcolor = pixel[j,i]
+			length = 1
+		else:
+			length += 1
+		#out.write(" setxy " + str(j) + " " + str(i) + " color " + str(16-pixel[j,i]) + " fd 1")
